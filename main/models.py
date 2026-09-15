138fe89fedca68a703ed5cdfb14a8a89c50c21f2
Nurbol <kadyrbekovnurbol002@gmail.com>
2026-09-15 19:32:36 +0600

    Биринчи проект

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    model = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
